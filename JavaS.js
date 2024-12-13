b96615f53a3c9073be5c5e1632c87bdc3e6112cb
Dk <Zargakhu000@hsestudents.org>
2024-12-13 11:14:58 -0500

    Add files via upload

---
+++ JavaS.js
@@ -0,0 +1,36 @@
+var name = prompt("If you're not a bot, and want to Continue, type: oK?");
+console.log(name);
+alert("didn’T yoU Realize i’d kNow? you shouldn’t share a weBpage At any point until it’s Completed, "+ name);
+
+//var name_change = document.getElementById("page");
+////name_change.textContent = name + " is making this webpage.";
+
+//var note = document.createElement("p");
+//note.textContent = "Hi Mom how are you?";
+//document.body.appendChild(note);
+
+//var child = document.getElementById("page");
+//document.body.insertBefore(note, child);
+
+//note.setAttribute("class", "extraNote");
+//note.setAttribute("style", "fontsize: 25px");
+
+
+var name1 = document.createElement("h1");
+name1.textContent = "Please Make Your Selection Above";
+document.body.appendChild(name1);
+
+name1.setAttribute("style", "background-color: gray");
+
+//var i = 1;
+//function changeColor2(event){
+ //   if(document.body.style.backgroundColor != "red"){
+ //      document.body.style.backgroundColor = "red";
+//    }
+ //   i++;
+ //   if(i == 5){
+ //       clearInterval(intervalID);
+ //       console.log("This function ran 5 times")
+//    }
+//}
+//var intervalID = setInterval(changeColor2, 1000) 
